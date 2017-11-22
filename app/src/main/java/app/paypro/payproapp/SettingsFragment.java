f08d613067d1ead570b12223037092eb4faf6a53
ottegu <>
2017-11-22 10:25:57 +0100

    Fix issues to render and enable scrollMode always

--- app/src/main/java/app/paypro/payproapp/SettingsFragment.java
+++ app/src/main/java/app/paypro/payproapp/SettingsFragment.java
@@ -8,10 +8,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TableRow;
 
 public class SettingsFragment extends Fragment {
 
@@ -48,5 +50,41 @@ public class SettingsFragment extends Fragment {
         roundedBitmapDrawable.setCircular(true);
 
         avatarImage.setImageDrawable(roundedBitmapDrawable);
+
+        TableRow rowProfile;
+        rowProfile = getView().findViewById(R.id.rowProfile);
+        rowProfile.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Log.i("eeeeee", "0000000000");
+            }
+        } );
+
+        TableRow rowPasscode;
+        rowPasscode = getView().findViewById(R.id.rowPasscode);
+        rowPasscode.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Log.i("eeeeee", "11111111111");
+            }
+        } );
+
+        TableRow rowTell;
+        rowTell = getView().findViewById(R.id.rowTell);
+        rowTell.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Log.i("eeeeee", "2222222222");
+            }
+        } );
+
+        TableRow rowInfo;
+        rowInfo = getView().findViewById(R.id.rowInfo);
+        rowInfo.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                Log.i("eeeeee", "33333333333");
+            }
+        } );
     }
 }
