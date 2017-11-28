d428457ccea000362ba2f412e1b994483dd4d416
Roger Baiget <rogerbaiget@Mac-mini-de-Roger.local>
2017-11-28 10:40:29 +0100

    Scan implemented

--- app/src/main/java/app/paypro/payproapp/ScanFragment.java
+++ app/src/main/java/app/paypro/payproapp/ScanFragment.java
@@ -1,12 +1,37 @@
 package app.paypro.payproapp;
 
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+import static android.Manifest.permission_group.CAMERA;
 
 public class ScanFragment extends Fragment {
+
+    private SurfaceView cameraView;
+    private CameraSource cameraSource;
+    private RelativeLayout mainView;
+    private static final int CAMERA_REQUEST_CODE = 102;
+
     public static ScanFragment newInstance() {
         ScanFragment fragment = new ScanFragment();
         return fragment;
@@ -21,5 +46,92 @@ public class ScanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.scan_tab, container, false);
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        cameraView = getActivity().findViewById(R.id.camera_view);
+        mainView = getActivity().findViewById(R.id.main_view);
+
+        BarcodeDetector barcodeDetector =
+                new BarcodeDetector.Builder(getContext())
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        cameraSource = new CameraSource
+                .Builder(getContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                startCamera();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                  String qrcode = barcodes.valueAt(0).displayValue;
+                }
+            }
+        });
+    }
+
+    @Override
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted , Access contacts here or do whatever you need.
+                startCamera();
+            }
+            return;
+        }
+    }
+
+    public void startCamera(){
+        try {
+            if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                        android.Manifest.permission.CAMERA)) {
+                    Snackbar.make(mainView, "Permission to access camera", Snackbar.LENGTH_INDEFINITE)
+                            .setAction(android.R.string.ok, new View.OnClickListener() {
+                                @Override
+                                @TargetApi(Build.VERSION_CODES.M)
+                                public void onClick(View v) {
+                                    ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                                }
+                            });
+
+                }else{
+                    ActivityCompat.requestPermissions(getActivity(), new String[] {android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                }
+            }else{
+                cameraSource.start(cameraView.getHolder());
+            }
+        } catch (IOException ie) {
+        }
     }
 }
